36a806245c1d53e34c444e39c7f41cc6cb4a9b7d
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-18 16:25:11 -0500

    moved root route to reference welcome.html, new template

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("root.html")
+def landing_page():
+    return render_template("welcome.html")
 
 if __name__ == "__main__":
     app.debug = True
